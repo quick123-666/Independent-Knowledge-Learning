0f37a6026c84fc17e7d9c783a1721e8f1d600855
IKL Deploy <deploy@ikl.com>
2026-06-14 21:01:32 +0800

    Initial commit: Independent Knowledge Learning platform

--- node_modules/urllib/dist/esm/IncomingHttpHeaders.d.ts
+++
@@ -1,5 +0,0 @@
-import type { IncomingHttpHeaders as HTTPIncomingHttpHeaders } from 'node:http';
-import type { Except } from 'type-fest';
-export interface IncomingHttpHeaders extends Except<HTTPIncomingHttpHeaders, 'set-cookie'> {
-    'set-cookie'?: string | string[];
-}
--- node_modules/urllib/src/formstream.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'formstream' {
-  export default class FormStream {
-    headers(): Record<string, string>;
-    file(name: string, path: string): void;
-    field(name: string, value: string): void;
-  }
-}
